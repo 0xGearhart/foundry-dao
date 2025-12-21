0240597d954ecc220031f7d5cd19c3b596b82991
gearhart <geardev26@gmail.com>
2025-12-21 15:49:16 -0800

    unit test file setup

---
+++ test/unit/SimpleStorageTest.t.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+
+import {SimpleStorage} from "../../src/SimpleStorage.sol";
+import {Test} from "forge-std/Test.sol";
+
+pragma solidity ^0.8.27;
+
+contract SimpleStorageTest is Test {
+    SimpleStorage public simpleStorage;
+
+    function setUp() public {}
+}
