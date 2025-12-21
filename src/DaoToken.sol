5938689f0f4951637bed291a760e4810e593ef36
gearhart <geardev26@gmail.com>
2025-12-21 15:30:10 -0800

    add token contract

---
+++ src/DaoToken.sol
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.27;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {ERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+import {ERC20Votes} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
+import {Nonces} from "@openzeppelin/contracts/utils/Nonces.sol";
+
+contract DaoToken is ERC20, ERC20Permit, ERC20Votes {
+    constructor(
+        address _tokenReceiver,
+        string memory _name,
+        string memory _symbol,
+        uint256 _initialSupply
+    )
+        ERC20(_name, _symbol)
+        ERC20Permit(_name)
+    {
+        _mint(_tokenReceiver, _initialSupply);
+    }
+
+    function clock() public view override returns (uint48) {
+        return uint48(block.timestamp);
+    }
+
+    // solhint-disable-next-line func-name-mixedcase
+    function CLOCK_MODE() public pure override returns (string memory) {
+        return "mode=timestamp";
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _update(address from, address to, uint256 value) internal override(ERC20, ERC20Votes) {
+        super._update(from, to, value);
+    }
+
+    function nonces(address owner) public view override(ERC20Permit, Nonces) returns (uint256) {
+        return super.nonces(owner);
+    }
+}
+
