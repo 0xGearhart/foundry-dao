c7fcd0b728285c6322ea12798ff0206bfb0ce01a
gearhart <geardev26@gmail.com>
2025-12-21 16:06:16 -0800

    refactor

--- src/DaoTimeLock.sol
+++ src/DaoTimeLock.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.27;
 
 import {TimelockController} from "@openzeppelin/contracts/governance/TimelockController.sol";
 
-contract TimeLock is TimelockController {
+contract DaoTimeLock is TimelockController {
     constructor(
         uint256 minDelay, // how long you have to wait before executing
         address[] memory proposers, // list of addresses that can propose
