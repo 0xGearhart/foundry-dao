e3923b7357ca4450ab94d8057615997461eaa686
gearhart <geardev26@gmail.com>
2025-12-22 15:57:58 -0800

    daoStorage tests

--- test/unit/DaoStorageTest.t.sol
+++ test/unit/DaoStorageTest.t.sol
@@ -1,17 +1,35 @@
 // SPDX-License-Identifier: MIT
 
-import {DaoStorage} from "../../src/DaoStorage.sol";
+import {DaoStorage, Ownable} from "../../src/DaoStorage.sol";
 import {Test} from "forge-std/Test.sol";
 
 pragma solidity ^0.8.27;
 
 contract DaoStorageTest is Test {
+    event NumberChanged(uint256 indexed newNumber);
+
     DaoStorage public daoStorage;
 
-    address USER = makeAddr("user");
-    address OWNER = DEFAULT_SENDER;
+    uint256 newNumber = 42;
+    address user = makeAddr("user");
+    address owner = DEFAULT_SENDER;
 
     function setUp() public {
-        daoStorage = new DaoStorage(OWNER);
+        daoStorage = new DaoStorage(owner);
+    }
+
+    function testNonOwnerCanNotChangeNumber() public {
+        vm.expectRevert(abi.encodeWithSelector(Ownable.OwnableUnauthorizedAccount.selector, user));
+        vm.prank(user);
+        daoStorage.changeNumber(newNumber);
+    }
+
+    function testOwnerCanChangeNumber() public {
+        assertEq(daoStorage.getNumber(), 0);
+        vm.expectEmit(true, false, false, false, address(daoStorage));
+        emit NumberChanged(newNumber);
+        vm.prank(owner);
+        daoStorage.changeNumber(newNumber);
+        assertEq(daoStorage.getNumber(), newNumber);
     }
 }
