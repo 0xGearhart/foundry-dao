bd170bb106e51cc0cce44656d580fd6b79c6d0c9
gearhart <geardev26@gmail.com>
2025-12-23 01:28:17 -0800

    added NatSpec documentation

--- src/DaoTimeLock.sol
+++ src/DaoTimeLock.sol
@@ -1,15 +1,26 @@
 // SPDX-License-Identifier: MIT
-// Compatible with OpenZeppelin Contracts ^5.5.0
+
 pragma solidity ^0.8.27;
 
 import {TimelockController} from "@openzeppelin/contracts/governance/TimelockController.sol";
 
+/**
+ * @title DaoTimeLock
+ * @author Dustin Gearhart
+ * @notice Time lock contract to ensure a minimum amount of time passes before any successful proposal is executed.
+ * This allows unsatisfied holders sufficient time to leave the DAO before proposal execution. Ensures all
+ * proposals are sent from the DAO. Permits any address to execute valid proposals after required delay has passed.
+ * @dev Compatible with OpenZeppelin Contracts ^5.5.0
+ */
 contract DaoTimeLock is TimelockController {
+    /*//////////////////////////////////////////////////////////////
+                               FUNCTIONS
+    //////////////////////////////////////////////////////////////*/
     constructor(
         uint256 minDelay, // how long you have to wait before executing
         address[] memory proposers, // list of addresses that can propose
         address[] memory executors, // list of addresses that can execute
-        address admin
+        address admin // address to grant administrator role to
     )
         TimelockController(minDelay, proposers, executors, admin)
     {}
